3584b61537bcd58cd37a478de5a4e7c641ce8688
Bbennn <sihombingbenjamin2004@gmail.com>
2023-11-15 15:10:56 +0700

    fix: nambahin .

--- pemain.pl
+++ pemain.pl
@@ -1,12 +1,12 @@
 /* Fakta dan Relasi */
-initDistribusi(X,Y)
-initDistribusi(2,24)
-initDistribusi(3,26)
-initDistribusi(4,12)
+initDistribusi(X,Y).
+initDistribusi(2,24),
+initDistribusi(3,26),
+initDistribusi(4,12),
 
 /* Dynamic predicate */
-dynamic(nPemain/1)      
-dynamic(NameTurn/2)     
+dynamic(nPemain/1).      
+dynamic(NameTurn/2).     
 
 /* Rule */
-throw2Dice(X)           
+throw2Dice(X).           
--- turn.pl
+++ turn.pl
@@ -1,15 +1,15 @@
 /* Fakta dan Relasi */
-bonusTentara(X,Y) 
-bonusTentara(amerikautara,3)
-bonusTentara(amerikaselatan,2)
-bonusTentara(eropa,3)
-bonusTentara(asia,5)
-bonusTentara(afrika,2)
-bonusTentara(australia,1)
+bonusTentara(X,Y). 
+bonusTentara(amerikautara,3).
+bonusTentara(amerikaselatan,2).
+bonusTentara(eropa,3).
+bonusTentara(asia,5).
+bonusTentara(afrika,2).
+bonusTentara(australia,1).
 
-maxMove(3)
+maxMove(3).
 
 /* Rule */
 endTurn         
-draft(X,Y)   
-move(X1,X2,Y)
+draft(X,Y).   
+move(X1,X2,Y).
