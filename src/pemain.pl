19bd12a45e4c542dfc7bc3af81db9927013cca17
ChaiGans <elbertchailes888@gmail.com>
2023-11-27 01:12:52 +0700

    feat: checkPlayerTerritories, mapInformation, playerInformation

--- src/pemain.pl
+++ src/pemain.pl
@@ -1,3 +1,5 @@
+:- include('initiating.pl').
+
 /* Fakta dan Relasi */
 initDistribusi(X,Y).
 initDistribusi(2,24).
@@ -5,17 +7,57 @@ initDistribusi(3,26).
 initDistribusi(4,12).
 
 /* Dynamic predicate */
-dynamic(nPemain/1).
+:- dynamic(nPemain/1).
 
-/* Nama pemain, Troops Aktif, Troops Tambahan, Jumlah Wilayah yang dimiliki, Jumlah Benua yang dimiliki*/
-dynamic(playerInformation/5). 
-/ Nama pemilik, Kode wilayah, Troops pemilik pada wilayah tersebut, */
-dynamic(mapInformation/4).
-dynamic(NameTurn/2).     
+
+/* Nama pemain, Troops Aktif, Troops Tambahan, Wilayah yang dimiliki, Benua yang dimiliki (maksudnya wilayahnya ada di benua mana aja)*/
+:- dynamic(playerInformation/5). 
+/ Nama pemilik, Kode wilayah, Troops pemilik pada wilayah tersebut*/
+:- dynamic(mapInformation/3).
+:- dynamic(NameTurn/2).     
 
 /* Rule */
 throw2Dice(X).           
 
-checkPlayerTerritories(p3) :-
+checkPlayerTerritories(PlayerLabel) :-
+    (   (PlayerLabel == 'p1') -> 
+        Label is 1
+    ;   (PlayerLabel == 'p2') -> 
+        Label is 2
+    ;   (PlayerLabel == 'p3') -> 
+        Label is 3
+    ;   (PlayerLabel == 'p4') -> 
+        Label is 4
+    ),
+
     write('Nama :'),
-    write 
+    labelpemain(PlayerName, Label),
+    write(PlayerName), nl,
+    playerInformation(PlayerName,_,_,_,ListOfContinent),
+    detailsOfContinent(ListOfContinent).
+
+detailsOfContinent([]).
+detailsOfContinent([NamaBenua | Tail], X) :-
+    write('Benua '),
+    write(NamaBenua),
+    write(' ('),
+    findall(NamaBenua, partBenua(Kode,NamaBenua), ListKodeWilayah),
+    listLength(ListKodeWilayah, Y),
+    write(Y),
+    write('/'),
+    findall(Wilayah, partBenua(Wilayah, NamaBenua), ListOfWilayah),
+    listLength(ListOfWilayah,BanyakWilayah),
+    write(BanyakWilayah),
+    write(')'),
+    printListKodeWilayah(ListKodeWilayah),
+    detailsOfContinent(Tail).
+
+printListKodeWilayah([]).
+printListKodeWilayah([KodeWilayah | Tail]) :-
+    write(KodeWilayah), nl,
+    % Assume namaWilayah/2 is defined to fetch the name of the territory
+    namaWilayah(KodeWilayah, NamaWilayah),
+    write('Nama : '), write(NamaWilayah), nl,
+    mapInformation(_, KodeWilayah, JumlahTentara),
+    write('Jumlah tentara : '), write(JumlahTentara), nl,
+    printListKodeWilayah(Tail).
