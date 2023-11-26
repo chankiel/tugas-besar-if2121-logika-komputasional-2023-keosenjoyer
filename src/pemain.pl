3e66a550d4dc2ca025a58812902452478f3e0e24
ChaiGans <elbertchailes888@gmail.com>
2023-11-26 21:26:19 +0700

    add: globalize dynamic predicate

--- src/pemain.pl
+++ src/pemain.pl
@@ -5,8 +5,17 @@ initDistribusi(3,26).
 initDistribusi(4,12).
 
 /* Dynamic predicate */
-dynamic(nPemain/1).      
+dynamic(nPemain/1).
+
+/* Nama pemain, Troops Aktif, Troops Tambahan, Jumlah Wilayah yang dimiliki, Jumlah Benua yang dimiliki*/
+dynamic(playerInformation/5). 
+/ Nama pemilik, Kode wilayah, Troops pemilik pada wilayah tersebut, */
+dynamic(mapInformation/4).
 dynamic(NameTurn/2).     
 
 /* Rule */
 throw2Dice(X).           
+
+checkPlayerTerritories(p3) :-
+    write('Nama :'),
+    write 
